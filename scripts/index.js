eea818aecc8031f0eff2a05941b530ff33470acd
Minin Vitaliy <mini.vitali@yandex.ru>
2022-06-28 19:15:22 +0300

    fix: доработан код по замечаниям Ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,59 +1,56 @@
-//поиск классов - открытие и закрытие формы
+/**поиск классов - открытие и закрытие формы*/
 const popup = document.querySelector('.popup');
 const profile = document.querySelector('.profile')
 const buttonOpenFormEditing = profile.querySelector('.profile__edit-button');
 const buttonCloseFormEditing = popup.querySelector('.popup__button-close');
 
-//поиск классов для сохранения изменений в форму
-let formElement = document.querySelector('.popup');
-const nameInput = popup.querySelector('.popup__form_name');
-const jobInput = popup.querySelector('.popup__form_job');
+/**поиск классов для сохранения изменений в форму*/
+let formElement = document.querySelector('.popup__forms');
+const nameInput = popup.querySelector('.popup__form_new_name');
+const jobInput = popup.querySelector('.popup__form_new_job');
+
+let saveName = profile.querySelector('.profile__title');
+let saveJob = profile.querySelector('.profile__text');
 
 
-//ввод функций
-//открытие формы
+/**ввод функций
+открытие формы*/
 function openedFormToPopup () {
     popup.classList.add('popup_opened');
-    console.log('добавлен класс');
+    /** присвоение значения указаных на странице в value для каждого title и text*/
+    nameInput.value = saveName.textContent;
+    jobInput.value = saveJob.textContent;
 };
-//закрытие формы
+
+/**закрытие формы*/
 function closedFormToPopup () {
     popup.classList.remove('popup_opened');
-    console.log('убран класс');
 };
 
-//сохранения формы
-function formSubmitHandler (evt) {
+/**сохранения формы*/
+function saveformSubmitHandler (evt) {
     evt.preventDefault(); 
-    nameInput.value;
-    console.log(nameInput.value);
-    jobInput.value;
-    console.log(jobInput.value);
-    // Выберите элементы, куда должны быть вставлены значения полей
-    let name = profile.querySelector('.profile__title');
-    let job = profile.querySelector('.profile__text');
-
-    // Вставьте новые значения с помощью textContent
-    name.textContent = nameInput.value;
-    job.textContent =  jobInput.value;
-
+    /** Вставьте новые значения с помощью textConten*/
+    saveName.textContent = nameInput.value;
+    saveJob.textContent =  jobInput.value;
     closedFormToPopup ()
 };
-//функция закрытия при совпадения target и currentTarget
+
+/** функция закрытия при совпадения target и currentTarget
 const closePpupopOverlay = function (event) {
     if (event.target !== event.currentTarget) {
      return
     }
     closedFormToPopup () 
- }
+};*/
 
- 
-//реакция на действия пользователя
-//открытие
+/**реакция на действия пользователя
+открытие*/
 buttonOpenFormEditing.addEventListener('click', openedFormToPopup);
-//закрытие
+/** закрытие */
 buttonCloseFormEditing.addEventListener('click', closedFormToPopup);
-//сохранения
-formElement.addEventListener('submit', formSubmitHandler); 
-//закрытие при нажатие вне формы (на затемненый экран)
-popup.addEventListener('click', closePpupopOverlay);
+/** сохранения */
+formElement.addEventListener('submit', saveformSubmitHandler); 
+
+/** закрытие при нажатие вне формы (на затемненый экран)
+popup.addEventListener('click', closePpupopOverlay);*/
